d51db765781c230bb9e8cde089c3bdb322f07d32
ferchopuertas <138713637+ferchopuertas@users.noreply.github.com>
2024-09-16 20:57:42 -0300

    Update login.jsx

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        const response = await fetch('https://backend-claerror404.onrender.com/api/login', {
+        const response = await fetch('https://backend-error404.onrender.com/api/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
